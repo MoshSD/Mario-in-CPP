52eac8735176100d98fea187d85cb1578d087cd2
MoshSD <62759350+MoshSD@users.noreply.github.com>
2021-02-10 15:01:54 +0000

    -Finished tutorial 6

--- MarioBaseProject/MarioBaseProject/MarioBase.cpp
+++ MarioBaseProject/MarioBaseProject/MarioBase.cpp
@@ -6,6 +6,7 @@
 #include "constants.h"
 #include "Texture2D.h"
 #include "Commons.h"
+#include "GameScreenManager.h"
 
 
 using namespace std;
@@ -15,7 +16,9 @@ using namespace std;
 //Globals
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
-Texture2D* g_texture = nullptr;
+GameScreenManager* game_screen_manager;
+Uint32 g_old_time;
+//Texture2D* g_texture = nullptr;
 
 //Function prototypes
 bool InitSDL();
@@ -32,6 +35,12 @@ int main(int argc, char* args[])
 	//check if SDL was setup correctly
 	if (InitSDL())
 	{
+		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
+		//set the time
+		g_old_time = SDL_GetTicks();
+
+
+
 		//flag to check if we wish to quit
 		bool quit = false;
 
@@ -91,12 +100,12 @@ bool InitSDL()
 	}
 
 	//Load the background texture
-	g_texture = new Texture2D(g_renderer);
+	//g_texture = new Texture2D(g_renderer);
 
-	if (!g_texture->LoadFromFile("Images/maxresdefault.jpg"))
-	{
-		return false;
-	}
+	//if (!g_texture->LoadFromFile("Images/maxresdefault.jpg"))
+	//{
+	//	return false;
+	//}
 	
 
 }
@@ -112,16 +121,23 @@ void CloseSDL()
 	SDL_Quit();
 
 	//release the texture
-	delete g_texture;
-	g_texture = nullptr;
+	//delete g_texture;
+	//g_texture = nullptr;
 
 	//release the renderer
 	SDL_DestroyRenderer(g_renderer);
 	g_renderer = nullptr;
+
+	//destroy the game screen manager
+	delete game_screen_manager;
+	game_screen_manager = nullptr;
+
 }
 
 bool Update()
 {
+	Uint32 new_time = SDL_GetTicks();
+
 	//Event handler
 	SDL_Event e;
 
@@ -137,6 +153,8 @@ bool Update()
 		break;
 	}
 
+	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
+	g_old_time = new_time;
 	return false;
 }
 
@@ -146,7 +164,8 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_renderer);
 
-	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	//g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	game_screen_manager->Render();
 
 	//update the screen
 	SDL_RenderPresent(g_renderer);
